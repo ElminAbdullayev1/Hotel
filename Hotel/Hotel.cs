f8984c1b9c743a9d0c0e4da4b022a74504c68af3
ElminAbdullayev1 <elminabdullayev46@gmail.com>
2025-04-23 03:59:17 +0400

    Menu yoxdu

---
+++ Hotel/Hotel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    class Hotel
+    {
+        public int Id { get; }
+        public string Name { get; }
+        private List<Room> Rooms { get; }
+
+        public Hotel(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentException("Hotel adı mütləqdir!");
+
+            Id = ++Id;
+            Name = name;
+            Rooms = new List<Room>();
+        }
+
+        public void AddRoom(Room room)
+        {
+            Rooms.Add(room);
+        }
+
+        public List<Room> FindAllRooms(Func<Room, bool> predicate)
+        {
+            return Rooms.Where(predicate).ToList();
+        }
+
+        public void MakeReservation(int? roomId, int guestCount)
+        {
+            if (roomId == null)
+                throw new NullReferenceException("Room ID null ola bilməz!");
+
+            Room room = Rooms.FirstOrDefault(r => r.Id == roomId);
+
+            if (room == null)
+                throw new ArgumentException("Göstərilən ID ilə otaq tapılmadı!");
+
+            if (!room.IsAvailable)
+                throw new NotAvailableException("Otaq artıq rezerv edilib!");
+
+            if (guestCount > room.PersonCapacity)
+                throw new ArgumentException("Müştəri sayı otağın tutumundan çox ola bilməz!");
+
+            room.Reserve();
+        }
+    }
+}
+}
---
+++ Hotel/Room.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    class Room
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public decimal Price { get; }
+        public int PersonCapacity { get; }
+        public bool IsAvailable { get; private set; }
+        public Room(string name, decimal price, int personCapacity)
+        {
+            if (string.IsNullOrEmpty(name) || price <= 0 || personCapacity <= 0)
+                throw new ArgumentException();
+
+            Id = ++Id;
+            Name = name;
+            Price = price;
+            PersonCapacity = personCapacity;
+            IsAvailable = true;
+        }
+        public string ShowInfo()
+        {
+            return $"Room ID: {Id}, Name: {Name}, Price: {Price}, Capacity: {PersonCapacity}, Available: {IsAvailable}";
+        }
+
+        public override string ToString()
+        {
+            return ShowInfo();
+        }
+
+        public void Reserve()
+        {
+            if (!IsAvailable)
+                throw new NotAvailableException("Bu otaq artıq rezerv edilib!");
+            IsAvailable = false;
+        }
+
+    }
+}
+
